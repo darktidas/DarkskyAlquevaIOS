8d2769efdf4fff3a1964bf41c4053d87b08b3e5e
tiago <darktidas@tiagos-mac.local>
2016-08-17 14:23:20 +0100

    Added download functions

--- DarkskyAlquevaIOS/ViewController.swift
+++ DarkskyAlquevaIOS/ViewController.swift
@@ -8,18 +8,91 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, NSURLSessionDownloadDelegate, UIDocumentInteractionControllerDelegate  {
+    
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    var downloadTask: NSURLSessionDownloadTask!
+    var backgroundSession: NSURLSession!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib.
+        let xml = XmlReader()
+        xml.donwload()
+        
+        
+        let backgroundSessionConfiguration = NSURLSessionConfiguration.backgroundSessionConfigurationWithIdentifier("backgroundSession")
+        backgroundSession = NSURLSession(configuration: backgroundSessionConfiguration, delegate: self, delegateQueue: NSOperationQueue.mainQueue())
+        
+        progressView.setProgress(0.0, animated: false)
+        
+        let url = NSURL(string: "http://publications.gbdirect.co.uk/c_book/thecbook.pdf")!
+        downloadTask = backgroundSession.downloadTaskWithURL(url)
+        downloadTask.resume()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    // 1
+    func URLSession(session: NSURLSession,
+                    downloadTask: NSURLSessionDownloadTask,
+                    didFinishDownloadingToURL location: NSURL){
+        
+        let path = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+        let documentDirectoryPath:String = path[0]
+        let fileManager = NSFileManager()
+        let destinationURLForFile = NSURL(fileURLWithPath: documentDirectoryPath.stringByAppendingString("/file.pdf"))
+        
+        if fileManager.fileExistsAtPath(destinationURLForFile.path!){
+            showFileWithPath(destinationURLForFile.path!)
+        }
+        else{
+            do {
+                try fileManager.moveItemAtURL(location, toURL: destinationURLForFile)
+                // show file
+                showFileWithPath(destinationURLForFile.path!)
+            }catch{
+                print("An error occurred while moving file to destination url")
+            }
+        }
+    }
+    // 2
+    func URLSession(session: NSURLSession,
+                    downloadTask: NSURLSessionDownloadTask,
+                    didWriteData bytesWritten: Int64,
+                                 totalBytesWritten: Int64,
+                                 totalBytesExpectedToWrite: Int64){
+        progressView.setProgress(Float(totalBytesWritten)/Float(totalBytesExpectedToWrite), animated: true)
+    }
+    
+    func showFileWithPath(path: String){
+        let isFileFound:Bool? = NSFileManager.defaultManager().fileExistsAtPath(path)
+        if isFileFound == true{
+            let viewer = UIDocumentInteractionController(URL: NSURL(fileURLWithPath: path))
+            viewer.delegate = self
+            viewer.presentPreviewAnimated(true)
+        }
+    }
+    
+    func documentInteractionControllerViewControllerForPreview(controller: UIDocumentInteractionController) -> UIViewController{
+        return self
+    }
+    
+    func URLSession(session: NSURLSession,
+                    task: NSURLSessionTask,
+                    didCompleteWithError error: NSError?){
+        downloadTask = nil
+        progressView.setProgress(0.0, animated: true)
+        if (error != nil) {
+            print(error?.description)
+        }else{
+            print("The task finished transferring data successfully")
+        }
+    }
 
 }
 
---
+++ DarkskyAlquevaIOS/XmlReader.swift
@@ -0,0 +1,16 @@
+//
+//  XmlReader.swift
+//  DarkskyAlquevaIOS
+//
+//  Created by tiago  on 08/08/16.
+//  Copyright © 2016 tiago . All rights reserved.
+//
+
+import Foundation
+
+class XmlReader {
+    
+    func donwload() {
+        print("jaciara")
+    }
+}
